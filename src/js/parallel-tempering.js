82e31ff0a286a2342c82ba5cde582a8d11b4e265
twined quartz <fmorisato@gmail.com>
2018-07-08 10:58:33 +0900

    rename virtualFile object

--- src/js/parallel-tempering.js
+++ src/js/parallel-tempering.js
@@ -11,7 +11,7 @@
   }
 }(this, function (_mt, _JSZip) {
 
-  const localFile = {};
+  const virtualFile = {};
 
   const mt = (typeof require === 'undefined' && (typeof _mt === 'object' || typeof _mt === 'function'))
     ? _mt
@@ -26,10 +26,10 @@
   const fs = (typeof require === 'undefined')
     ? {
       writeFile: async (path, content) => {
-        localFile[path] = [content]
+        virtualFile[path] = [content]
       },
       appendFile: async (path, content) => {
-        localFile[path].push(content)
+        virtualFile[path].push(content)
       }
     }
     : require("fs.promises")
@@ -529,7 +529,7 @@
       const zip = new JSZip();
       const ts = [...this.timestamp].pop()
       const folder = zip.folder(ts);
-      Object.entries(localFile)
+      Object.entries(virtualFile)
         .filter(([k, v]) => k.match(ts) !== null)
         .map(([k, v]) => {
           folder.file(k, v.reduce((a, b) => a + b))
