6b2e9e07111c313f529b6d06a29d2de07a0c4996
twined quartz <fmorisato@gmail.com>
2018-07-20 17:32:46 +0900

    サンプラーモデルを実行可能なことを確認

--- model/model-sampler.js
+++ model/model-sampler.js
@@ -11,26 +11,30 @@
 
     const mode = 'sampler';
 
+    const Gauss = (mu, sigma) => x => {
+      return 1 / (Math.sqrt(2 * Math.PI) * sigma)
+        * Math.exp(
+          - ((x - mu) * (x - mu)) / (2 * sigma * sigma)
+        )
+    }
+    const gauss1 = Gauss(1, 1);
+    const gauss2 = Gauss(5, 2);
+    const gauss3 = Gauss(-2, 3);
+
     const model = (parameters, _) => {
-      const { x, y } = parameters[0];
-      const f = Math.exp(-x * x) * Math.exp(-y * y);
+      const { x } = parameters[0];
+      const f = gauss1(x) + gauss2(x) + gauss3(x);
       return f
     };
 
     const parameters = [
       {
-        'x': 0,
-        'y': 0
+        'x': 0
       }
     ];
 
     const updateCondition = {
       'x': {
-        'max': Infinity,
-        'min': -Infinity,
-        'val': 1
-      },
-      'y': {
         'max': Infinity,
         'min': -Infinity,
         'val': 1
