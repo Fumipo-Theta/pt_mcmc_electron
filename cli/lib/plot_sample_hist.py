97a9089fa36b409070c4783e589f424aa61b9958
twined quartz <fmorisato@gmail.com>
2019-12-17 09:19:12 +0900

    Make seeing easier
    expand label fontsize, shrink margin between subplots

--- cli/lib/plot_sample_hist.py
+++ cli/lib/plot_sample_hist.py
@@ -13,6 +13,14 @@ def cut_significant(column, p_val: float):
     return apply
 
 
+def check_last_of(total_len):
+    return lambda i: i == total_len-1
+
+
+def is_first(i):
+    return i == 0
+
+
 def plot(
     mcmc: MCMCResult,
     burn_in: int,
@@ -29,19 +37,29 @@ def plot(
 
     figure = Figure()
 
-    for row, param in enumerate(params):
-        for i in range(n_set):
+    is_last_row = check_last_of(len(params))
+
+    for row_i, param in enumerate(params):
+        for column_i in range(n_set):
 
             subplot = Subplot(axes_style).add(
                 data=data,
-                y=cut_significant(f"{param}{i}", p_val),
-                ylabel=f"{label_map.get(param, param)} {i+1}",
+                y=cut_significant(f"{param}{column_i}", p_val),
                 ylim=lim_map.get(param, [None, None]),
                 plot=[
                     plot_action.hist(
                         bins=bins[param], density=True, color="black", orientation="horizontal")
-                ]
+                ],
+
+                ylabel=f"{label_map.get(param, param)}" if is_first(
+                    column_i) else None,
+                tick={
+                    "labelbottom": True if is_last_row(row_i) else False,
+                    "labelleft": True if is_first(column_i) else False,
+                    "right": True
+                },
+                title=f"Stage {column_i + 1}" if is_first(row_i) else None,
             )
             figure.add_subplot(subplot)
 
-    return figure.show(size=fig_style.get("size", (4, 3)), column=n_set)
+    return figure.show(**fig_style, column=n_set)
